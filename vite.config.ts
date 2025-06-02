ebbd264c8c51a18387b88441def62c7530e382d8
Poornima1029 <poornimavipparthi590@gmail.com>
2025-06-02 14:01:38 +0530

    update 1

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => ({
+  base: './',  // ✅ this is the fix
   plugins: [vue()],
   build: {
-    outDir: 'dist', // default, but explicitly stating it is safe
+    outDir: 'dist',
   },
   server: mode === 'development' ? {
     proxy: {
